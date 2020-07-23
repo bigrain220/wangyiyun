a45ed9bf939c00c97f984d9f3cf89008bd9883bd
william <1573223470@qq.com>
2020-07-23 15:35:05 +0800

    'promise'

---
+++ 异步编程/index.js
@@ -0,0 +1,34 @@
+// 代码一
+
+// console.log(1); //任务队列
+// setTimeout(() => {
+//     console.log(2) //任务队列
+// }, 0);
+// Promise.resolve().then(() => {
+//     console.log(3) //promise 属于微任务 
+// });
+// console.log(4); //任务队列
+
+
+// 1 4 3  2 
+// 整段script代码是一个宏任务,首先执行任务队列,打印1和4,然后执行微任务打印3,最后执行任务队列打印2
+
+
+// 代码二
+console.log('start');
+setTimeout(() => {
+    console.log('setTimeout');
+    new Promise(resolve => {
+        console.log('promise inner1');
+        resolve();
+    }).then(() => {
+        console.log('promise then1')
+    })
+}, 0)
+new Promise(resolve => {
+    console.log('promise inner2');
+    resolve();
+}).then(() => {
+    console.log('promise then2');
+});
+console.log('end');
---
+++ 异步编程/promise_test.js
@@ -0,0 +1,38 @@
+// 总结： 
+// 1.创建promise的简便方法： Promise.resolve(value); 等同于 new Promise((resolve, reject) => {resolve(value)});
+// 2.Promise里面要有resolve()，才能用then进行下一步，如果要传值，则resolve(value)
+// 3.then里面要写函数才有效果，而且如果要传值的话则需要 return value
+
+// 片段1
+const promise = new Promise((resolve, reject) => {
+    console.log(1);
+    resolve();
+    console.log(2);
+});
+
+promise.then(() => {
+    console.log(3);
+});
+console.log(4);
+
+//打印结果： 1 2 4 3   1 2 4 是线程任务同步执行，3是微任务里面打印的
+
+// 片段2
+const promise0 = Promise.
+resolve(1).
+then(2).
+then(Promise.resolve(3)).
+then(console.log);
+
+const promise1 = Promise.resolve(1); // resolve的值为1,做为下一个then的参数
+const promise2 = promise1.then(2); //then里面的不是函数，就会忽略，不起作用，依然resolve的值为1
+const promise3 = promise2.then(Promise.resolve(3)); //then里面的不是函数，就会忽略，不起作用，依然resolve的值为1
+// const promise4 = promise3.then(console.log);  //这边的console.log打印的是then接受到的参数
+//和下面的效果一样
+const promise4 = promise3.then(res => {
+    console.log(res);
+    return res
+    //如果then里面是函数，要有return值下一个then才能接受到。不然下一个then接受到的是undefined;   
+})
+
+//打印结果： 1 1 
